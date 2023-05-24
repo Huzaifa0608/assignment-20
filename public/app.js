b8547010ff21c7a3af6b4ad46ff57c29da7178e6
“Huzaifa0608” <huzaifafarhan7890@gmail.com>
2023-05-24 15:54:25 +0500

    Work Complete

---
+++ public/app.js
@@ -0,0 +1,37 @@
+var inp1 =document.getElementById("inp_1") 
+var inp2 =document.getElementById("inp_2")
+var inp3 =document.getElementById("inp_3")
+var inp4 =document.getElementById("inp_4")
+var inp5 = document.getElementById("dob")
+var inp6 = document.getElementById("opt_1")
+var inp7 = document.getElementById("yob")
+var inp8= document.getElementById("gender")
+
+
+
+
+
+
+
+
+
+
+
+
+// var inp1 = document.getElementById("text1")
+// var inp2 = document.getElementById("text2")
+// var inp3 = document.getElementById("text3")
+// var inp4 = document.getElementById("text4")
+
+
+function registration(){
+    console.log(inp1.value)
+    console.log(inp2.value)
+    console.log(inp3.value)
+    console.log(inp4.value)
+     console.log(inp5.value)
+    console.log(inp6.value)
+    console.log(inp7.value)
+    console.log(inp8.value)
+
+}
